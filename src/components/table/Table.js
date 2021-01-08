849b94b7fe5745ceb47679255a028cbd51ae58b5
michaeltaman <mictrwork@gmail.com>
2021-01-08 13:38:49 +0200

    Finish base table / formula logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,33 +1,82 @@
 import {ExcelComponent} from '@core/ExcelComponent'
-import {createTable} from '@/components/table/table.template'
-import {resizeHandler} from "@/components/table/table.resize";
-import {shouldResize} from "@/components/table/table.functions";
+import {$} from '@core/dom'
+import {createTable} from './table.template'
+import {resizeHandler} from "./table.resize";
+import {nextSelector, matrix, isCell, shouldResize} from "./table.functions";
+import {TableSelection} from "./TableSelection"
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
-      listeners: ['mousedown']
+      name:'Table',
+      listeners: ['mousedown', 'keydown', 'input'],
+      ...options
     })
+
   }
 
   toHTML() {
-    return createTable(20)
+    return createTable(Table.rows_limit)
+  }
+
+  prepare() {
+    this.selection = new TableSelection()
+  }
+
+  init() {
+    super.init()
+    const $cell = this.$root.find('[data-id="0:0"]')
+    this.selectCell($cell)
+
+    this.$on('formula:input', text => {
+      this.selection.current.text(text)
+    })
+
+    this.$on('formula:done', () => {
+      this.selection.current.focus()
+    })
   }
 
   onMousedown(event) {
     if(shouldResize(event)) {
       return resizeHandler(this.$root, event)
+    } else if(isCell(event)) {
+        const $target = $(event.target)
+        if(event.shiftKey) {
+          const $cells = matrix($target, this.selection.current)
+                        .map(id => this.$root.find(`[data-id = "${id}"]`))
+          this.selection.selectGroup($cells)
+        } else {
+            this.selection.select($target)
+            this.onInput(event)
+        }
     }
   }
+
+  onKeydown(event) {
+    const keys = ['Enter','Tab','ArrowLeft','ArrowRight','ArrowDown','ArrowUp']
+    const {key} = event
+    if(keys.includes(key) && !event.shiftKey) {
+      // to prevent page scrolling
+      event.preventDefault()
+      const id = this.selection.current.id(true)
+      const $next = this.$root.find(nextSelector(key, id))
+      this.selectCell($next)
+    }
+  }
+
+  onInput(event) {
+    this.$emit('table:input', $(event.target))
+  }
+
+  destroy() {
+    super.destroy()
+  }
+
+  selectCell($cell) {
+    this.selection.select($cell)
+    this.$emit('table:select', $cell)
+  }
 }
-
-// 589 msScripting
-// 2433 msRendering
-
-// 440 msScripting
-// 1771 msRendering
-
-// 125 msScripting
-// 329 msRendering
